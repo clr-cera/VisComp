1112473a1545aca17c03f4856e0d9bc597c803c6
clr <clrcera05@gmail.com>
2025-11-20 13:35:22 -0300

    authors!

--- final_project/website/main.py
+++ final_project/website/main.py
@@ -12,9 +12,32 @@ def main_page():
     with last_co:
         st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Ferris_trans_flag_pride.jpg/500px-Ferris_trans_flag_pride.jpg", width=400)
 
+
+def authors_page():
+    st.title("Autores")
+    st.write("Este projeto foi desenvolvido com muito carinho por:")
+    st.markdown("""
+    - Clara Ernesto de Carvalho - 14559479
+    - Felipe Carneiro Machado - 14569373  
+    - Renan Parpinelli Scarpin - 14712188  
+    - Gabriel Barbosa dos Santos - 14613991  
+    - Lourenço de Salles Roselino - 11796805
+                """)
+
+    st.write("Agradecemos a todos que contribuíram para a realização deste projeto!")
+    st.markdown(""" 
+                - Instituto de Ciências Matemáticas e de Computação - USP São Carlos
+                - Prof. Dr. Jean R. Ponciano - SCC0252 / SCC5836 – Visualização Computacional – 2025/2
+                """)
+    st.write("E claro, a você que está utilizando nossa aplicação!")
+    st.image("https://cdn-5a6cb102f911c811e474f1cd.closte.com/wp-content/uploads/2017/04/How-To-Make-A-Pizza-Pie-Chart.png", width=400)
+    
+
 if __name__ == "__main__":
 
     pages = [st.Page(main_page, title="Home"),
-             st.Page(page_general.render_page, title="Visualizações Gerais")]
+             st.Page(page_general.render_page, title="Visualizações Gerais"),
+             st.Page(authors_page, title="Autores")
+            ]
     pg = st.navigation(pages, position='top')
     pg.run()
